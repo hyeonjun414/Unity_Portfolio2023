3086f52ee38d59c11c3130447f4d92558348cf80
Hyeonjun <wns8363@gmail.com>
2023-03-22 13:38:20 +0900

    Update - JObject 리스트도 오브젝트로 변환가능하도록

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 public class Util
 {
+    public static List<T> ToObjectList<T>(List<JObject> jsonList)
+    {
+        var objList = new List<T>();
+        foreach (var jObj in jsonList)
+        {
+            var obj = ToObject<T>(jObj);
+            objList.Add(obj);
+        }
+
+        return objList;
+    }
     public static T ToObject<T>(JObject data)
     {
         var type = typeof(T);
