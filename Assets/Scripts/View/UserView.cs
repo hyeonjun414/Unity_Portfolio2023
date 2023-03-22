d9d18de695ef3c2a5021c580cd9d86818c7613f2
Hyeonjun <wns8363@gmail.com>
2023-03-22 13:17:18 +0900

    Update UserView.cs

--- Assets/Scripts/View/UserView.cs
+++ Assets/Scripts/View/UserView.cs
@@ -7,16 +7,10 @@ namespace View
 {
     public interface IUserView
     {
-        EntityModel GetHero();
     }
     public class UserView : MonoBehaviour, IUserView
     {
         public User Presenter;
 
-       
-        public EntityModel GetHero()
-        {
-            return Presenter.GetHero();
-        }
     }
 }
