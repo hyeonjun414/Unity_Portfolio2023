ac1003c047967ca9c499eea55f8046d95f705f35
Hyeonjun <wns8363@gmail.com>
2023-04-05 23:13:09 +0900

    Update - StageNode 위치 상하 좌우 정렬

--- Assets/Scripts/View/MapView.cs
+++ Assets/Scripts/View/MapView.cs
@@ -11,7 +11,7 @@ namespace View
 
         public StageNodeView nodePrefab;
         public List<List<StageNodeView>> StageNodeViews;
-        public Transform nodePivot;
+        public RectTransform nodePivot;
         public void Start()
         {
             if (GameManager.Instance == null)
@@ -24,14 +24,27 @@ namespace View
 
         public void GenerateStageNodes(List<List<Stage>> stages)
         {
+            // set contents size
+            var totalHeight = stages.Count * 200f;
+            nodePivot.sizeDelta = new Vector2(nodePivot.rect.width, totalHeight);
+            nodePivot.position = new Vector2(0, totalHeight / 2);
+            
             var stageNodes = new List<List<StageNodeView>>();
+            
+            
             for (var i = 0; i < stages.Count; i++)
             {
                 var stageStep = new List<StageNodeView>();
+                // Calculate the total width of this row of nodes
+                var totalWidth = stages[i].Count * 150f;
+
                 for (var j = 0; j < stages[i].Count; j++)
                 {
                     var node = Instantiate(nodePrefab, nodePivot);
-                    node.transform.localPosition += new Vector3(j * 150, i * 200, 0);
+                    node.transform.localPosition += new Vector3(
+                        (-totalWidth / 2) + (j + 0.5f) * 150f,
+                        (-totalHeight / 2) + i * 200f,
+                        0);
                     node.Init(stages[i][j]);
                     stageStep.Add(node);
                 }
